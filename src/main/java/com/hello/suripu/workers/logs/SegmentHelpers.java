a4e99a524fb7614e8eee297f8dc2288ef70a631c
Tim Bart <tim@sayhello.com>
2016-04-20 15:07:01 +0200

    Add logging

--- src/main/java/com/hello/suripu/workers/logs/SegmentHelpers.java
+++ src/main/java/com/hello/suripu/workers/logs/SegmentHelpers.java
@@ -1,11 +1,14 @@
 package com.hello.suripu.workers.logs;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.hello.suripu.core.metrics.DeviceEvents;
 import com.segment.analytics.messages.MessageBuilder;
 import com.segment.analytics.messages.TrackMessage;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Set;
@@ -19,6 +22,8 @@ public class SegmentHelpers {
     private final static String GESTURE_TRACK_NAME = "SenseGesture";
     private final static String ALARM_RING_TRACK_NAME = "SenseAlarmRing";
     private final static String ALARM_DISMISS_TRACK_NAME = "SenseAlarmDismiss";
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(SegmentHelpers.class);
 
     /**
      * Creates MessageBuilders to be sent to Segment
@@ -39,12 +44,14 @@ public class SegmentHelpers {
             // TODO: MAKE THIS CONFIGURABLE
             final List<MessageBuilder> ringMessages = tag(ImmutableMap.of("alarm","ring"), ALARM_RING_TRACK_NAME, accountsWhoseAlarmRang, deviceEvents.createdAt);
             messages.addAll(ringMessages);
+            LOGGER.info("action=tag-alarm-ring segment_count={} sense_id={} events={}", ringMessages.size(), deviceEvents.deviceId, Joiner.on(",").join(deviceEvents.events));
         }
 
         if(deviceEvents.events.contains(ALARM_DISMISSED_EVENT)) {
             // TODO: MAKE THIS CONFIGURABLE
             final List<MessageBuilder> ringMessages = tag(ImmutableMap.of("alarm","dismissed"), ALARM_DISMISS_TRACK_NAME, accountsWhoseAlarmRang, deviceEvents.createdAt);
             messages.addAll(ringMessages);
+            LOGGER.info("action=tag-alarm-dismiss segment_count={} sense_id={} events={}", ringMessages.size(), deviceEvents.deviceId, Joiner.on(",").join(deviceEvents.events));
         }
 
         // TO AVOID SPAMMING SEGMENT
@@ -52,6 +59,7 @@ public class SegmentHelpers {
             if(deviceEvents.events.contains(WAVE_EVENT)) {
                 final List<MessageBuilder> gestureMessages = tag(ImmutableMap.of("gesture","wave"), GESTURE_TRACK_NAME, pairedAccounts, deviceEvents.createdAt);
                 messages.addAll(gestureMessages);
+                LOGGER.info("action=tag-gesture segment_count={} sense_id={} events={}", gestureMessages.size(), deviceEvents.deviceId, Joiner.on(",").join(deviceEvents.events));
             }
         }
 

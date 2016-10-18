6bcaa82afc53488ddb4278da6955e211707a8fce
Josef Norgan <josef@sayhello.com>
2016-10-18 11:25:57 -0700

    Moved KMSConfig into core

--- src/main/java/com/hello/suripu/workers/expansions/AlarmActionWorkerConfiguration.java
+++ src/main/java/com/hello/suripu/workers/expansions/AlarmActionWorkerConfiguration.java
@@ -1,6 +1,7 @@
 package com.hello.suripu.workers.expansions;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hello.suripu.coredropwizard.configuration.KMSConfiguration;
 import com.hello.suripu.coredropwizard.configuration.NewDynamoDBConfiguration;
 import com.hello.suripu.coredropwizard.configuration.RedisConfiguration;
 import com.hello.suripu.workers.framework.WorkerConfiguration;

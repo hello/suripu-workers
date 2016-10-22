e30632e552f79fb6e2da34a7df0f960fb462cc87
kingshy <kingshy@sayhello.com>
2016-10-21 17:59:17 -0700

    rename config files, add metrics to supichi worker

--- src/main/java/com/hello/suripu/workers/supichi/SupichiRecordProcessorFactory.java
+++ src/main/java/com/hello/suripu/workers/supichi/SupichiRecordProcessorFactory.java
@@ -4,8 +4,11 @@ import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessor;
 import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorFactory;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.SSEAwsKeyManagementParams;
+import com.codahale.metrics.MetricRegistry;
 import com.hello.suripu.core.speech.interfaces.SpeechResultIngestDAO;
 import com.hello.suripu.core.speech.interfaces.SpeechTimelineIngestDAO;
+
+import static sun.awt.geom.Crossings.debug;
 
 /**
  * Created by ksg on 8/11/16
@@ -17,25 +20,31 @@ public class SupichiRecordProcessorFactory implements IRecordProcessorFactory {
     private final SSEAwsKeyManagementParams s3SSEKey;
     private final SpeechTimelineIngestDAO speechTimelineIngestDAO;
     private final SpeechResultIngestDAO speechResultIngestDAO;
-    private final Boolean debug;
+    private final Boolean logUUID;
+    private final Integer maxRecords;
+    private final MetricRegistry metrics;
 
     public SupichiRecordProcessorFactory(final String s3Bucket,
                                          final AmazonS3 s3,
                                          final SSEAwsKeyManagementParams s3SSEKey,
                                          final SpeechTimelineIngestDAO speechTimelineIngestDAO,
                                          final SpeechResultIngestDAO speechResultIngestDAO,
-                                         final Boolean debug) {
+                                         final Boolean logUUID,
+                                         final Integer maxRecords,
+                                         final MetricRegistry metrics) {
         this.s3 = s3;
         this.s3Bucket = s3Bucket;
         this.s3SSEKey = s3SSEKey;
         this.speechTimelineIngestDAO = speechTimelineIngestDAO;
         this.speechResultIngestDAO = speechResultIngestDAO;
-        this.debug = debug;
+        this.logUUID = logUUID;
+        this.maxRecords = maxRecords;
+        this.metrics = metrics;
     }
 
 
     @Override
     public IRecordProcessor createProcessor() {
-        return new SupichiRecordProcessor(s3, s3Bucket, s3SSEKey, speechTimelineIngestDAO, speechResultIngestDAO, debug);
+        return new SupichiRecordProcessor(s3, s3Bucket, s3SSEKey, speechTimelineIngestDAO, speechResultIngestDAO, logUUID, maxRecords, metrics);
     }
 }
